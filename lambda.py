e102d7ef5331706f83afa67d3fbd4b765bdaf030
Nicolas Vanheuverzwijn <nicolas.vanheu@gmail.com>
2018-02-16 22:38:15 -0500

    fix: various typo

--- lambda.py
+++ lambda.py
@@ -5,11 +5,12 @@ import time
 
 ec2 = boto3.client('ec2')
 volume_tag_namespace = os.environ['LAMBDA_VOLUME_TAG_NAMESPACE']
-lambda_arn = context.invoked_function_arn
-lambda_function_name = context.invoked_function_name
-lambda_function_version = context.invoked_function_version
 
 def lambda_handler(event, context):
+
+    lambda_arn = context.invoked_function_arn
+    lambda_function_name = context.function_name
+    lambda_function_version = context.function_version
 
     def find_all_eligible_volumes():
         print('Searching for volume with tag "%s"' % volume_tag_namespace)
@@ -25,14 +26,14 @@ def lambda_handler(event, context):
         return volumes
 
     def snapshot_volume(volume):
-            instance_id = volume['Attachments']['InstanceId']
+            instance_id = volume['Attachments'][0]['InstanceId']
             instance_tags = ec2.describe_tags(Filters=[{'Name': 'resource-id', 'Values': [instance_id]}])
             instance_name = find_tag(instance_tags, 'Name')
 
             volume_id = volume['VolumeId']
             volume_name = find_tag(volume, 'Name')
 
-            print("Found EBS volume %s (%s) on instance %s (%s), creating snapshot" % (volume_name, volume_id, instance_name, instance_id))
+            print("Found EBS volume %s (%s) on instance %s (%s)" % (volume_name, volume_id, instance_name, instance_id))
 
             snapshot = ec2.create_snapshot(
                 Description="%s from instance %s (%s)" % (volume_name or volume_id, instance_name, instance_id),
@@ -47,9 +48,9 @@ def lambda_handler(event, context):
                 {'Key': 'EbsBackup_InstanceId', 'Value': instance_id},
                 {'Key': 'EbsBackup_InstanceName', 'Value': instance_name},
                 {'Key': 'EbsBackup_VolumeName', 'Value': volume_name},
-                {'Key': 'EbsBackup_DeviceName', 'Value': volume['Attachments']['Device']},
+                {'Key': 'EbsBackup_DeviceName', 'Value': volume['Attachments'][0]['Device']},
                 {'Key': 'EbsBackup_Datetime', 'Value': today_string},
-                {'Key': 'EbsBackup_Timestamp', 'Value': time.time()},
+                {'Key': 'EbsBackup_Timestamp', 'Value': str(time.time())},
                 {'Key': 'EbsBackup_LambdaARN', 'Value': lambda_arn},
                 {'Key': 'EbsBackup_LambdaFunctionName', 'Value': lambda_function_name},
                 {'Key': 'EbsBackup_LambdaFunctionVersion', 'Value': lambda_function_version},
