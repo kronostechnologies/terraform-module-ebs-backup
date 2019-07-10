a4d5a13b9428ddcd519bacf195ccbaf323461795
Antoine Deschênes <antoine.deschenes@equisoft.com>
2019-07-10 15:32:23 -0400

    Terraform 0.12 support

--- main.tf
+++ main.tf
@@ -17,11 +17,12 @@ resource "aws_iam_role" "lambda" {
   ]
 }
 EOF
+
 }
 
 resource "aws_iam_role_policy" "lambda" {
   name = "ebs-backup-${var.lambda_aws_iam_role_name}"
-  role = "${aws_iam_role.lambda.id}"
+  role = aws_iam_role.lambda.id
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -51,6 +52,7 @@ resource "aws_iam_role_policy" "lambda" {
   ]
 }
 EOF
+
 }
 
 ##
@@ -67,13 +69,13 @@ resource "aws_lambda_function" "lambda" {
   runtime          = "python2.7"
   filename         = "${path.module}/lambda.zip"
   function_name    = "ebs-backup-${var.lambda_function_name}"
-  role             = "${aws_iam_role.lambda.arn}"
+  role             = aws_iam_role.lambda.arn
   handler          = "lambda.lambda_handler"
-  source_code_hash = "${data.archive_file.lambda.output_base64sha256}"
+  source_code_hash = data.archive_file.lambda.output_base64sha256
   timeout          = 3
   environment {
     variables = {
-      LAMBDA_VOLUME_TAG_NAMESPACE= "EbsBackup_TakeSnapshot_${var.lambda_volume_tag_namespace}"
+      LAMBDA_VOLUME_TAG_NAMESPACE = "EbsBackup_TakeSnapshot_${var.lambda_volume_tag_namespace}"
     }
   }
 }
@@ -81,35 +83,35 @@ resource "aws_lambda_function" "lambda" {
 resource "aws_lambda_permission" "cloudwatch" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.lambda.arn}"
+  function_name = aws_lambda_function.lambda.arn
   principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.lambda.arn}"
+  source_arn    = aws_cloudwatch_event_rule.lambda.arn
 }
 
 resource "aws_cloudwatch_event_rule" "lambda" {
   name                = "ebs-backup-${var.lambda_cloudwatch_event_name}"
-  schedule_expression = "${var.lambda_schedule_expression}"
+  schedule_expression = var.lambda_schedule_expression
 }
 
 resource "aws_cloudwatch_event_target" "lambda" {
   target_id = "ebs-backup-${var.lambda_cloudwatch_event_name}"
-  rule      = "${aws_cloudwatch_event_rule.lambda.name}"
-  arn       = "${aws_lambda_function.lambda.arn}"
+  rule      = aws_cloudwatch_event_rule.lambda.name
+  arn       = aws_lambda_function.lambda.arn
 }
 
 resource "aws_cloudwatch_metric_alarm" "lambda-ebs-backup-error" {
-  alarm_name                = "${aws_lambda_function.lambda.function_name}-error"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "1"
-  metric_name               = "Errors"
-  namespace                 = "AWS/Lambda"
-  period                    = "${var.lambda_alarm_period}"
-  statistic                 = "Sum"
-  threshold                 = "1"
-  alarm_description         = "This metric monitors error with lambda function '${aws_lambda_function.lambda-cleanup.function_name}'"
-  alarm_actions             = ["${var.lambda_alarm_actions}"]
-  dimensions {
-    FunctionName = "${aws_lambda_function.lambda.function_name}"
+  alarm_name          = "${aws_lambda_function.lambda.function_name}-error"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = var.lambda_alarm_period
+  statistic           = "Sum"
+  threshold           = "1"
+  alarm_description   = "This metric monitors error with lambda function '${aws_lambda_function.lambda-cleanup.function_name}'"
+  alarm_actions       = var.lambda_alarm_actions
+  dimensions = {
+    FunctionName = aws_lambda_function.lambda.function_name
   }
 }
 
@@ -127,14 +129,14 @@ resource "aws_lambda_function" "lambda-cleanup" {
   runtime          = "python3.6"
   filename         = "${path.module}/lambda-cleanup.zip"
   function_name    = "ebs-backup-cleanup-${var.lambda_function_name}"
-  role             = "${aws_iam_role.lambda.arn}"
+  role             = aws_iam_role.lambda.arn
   handler          = "lambda-cleanup.lambda_handler"
-  source_code_hash = "${data.archive_file.lambda-cleanup.output_base64sha256}"
+  source_code_hash = data.archive_file.lambda-cleanup.output_base64sha256
   timeout          = 300
   environment {
     variables = {
-      LAMBDA_VOLUME_TAG_NAMESPACE= "EbsBackup_TakeSnapshot_${var.lambda_volume_tag_namespace}",
-      LAMBDA_BACKUP_DAYS_TO_KEEP="${var.lambda_backup_days_to_keep}"
+      LAMBDA_VOLUME_TAG_NAMESPACE = "EbsBackup_TakeSnapshot_${var.lambda_volume_tag_namespace}"
+      LAMBDA_BACKUP_DAYS_TO_KEEP  = var.lambda_backup_days_to_keep
     }
   }
 }
@@ -142,34 +144,35 @@ resource "aws_lambda_function" "lambda-cleanup" {
 resource "aws_lambda_permission" "cloudwatch-cleanup" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.lambda-cleanup.arn}"
+  function_name = aws_lambda_function.lambda-cleanup.arn
   principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.lambda-cleanup.arn}"
+  source_arn    = aws_cloudwatch_event_rule.lambda-cleanup.arn
 }
 
 resource "aws_cloudwatch_event_rule" "lambda-cleanup" {
   name                = "ebs-backup-cleanup-${var.lambda_cloudwatch_event_name}"
-  schedule_expression = "${var.lambda_schedule_expression}"
+  schedule_expression = var.lambda_schedule_expression
 }
 
 resource "aws_cloudwatch_event_target" "lambda-cleanup" {
   target_id = "ebs-backup-cleanup-${var.lambda_cloudwatch_event_name}"
-  rule      = "${aws_cloudwatch_event_rule.lambda-cleanup.name}"
-  arn       = "${aws_lambda_function.lambda-cleanup.arn}"
+  rule      = aws_cloudwatch_event_rule.lambda-cleanup.name
+  arn       = aws_lambda_function.lambda-cleanup.arn
 }
 
 resource "aws_cloudwatch_metric_alarm" "lambda-cleanup-ebs-backup-error" {
-  alarm_name                = "${aws_lambda_function.lambda-cleanup.function_name}-error"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "1"
-  metric_name               = "Errors"
-  namespace                 = "AWS/Lambda"
-  period                    = "${var.lambda_alarm_period}"
-  statistic                 = "Sum"
-  threshold                 = "1"
-  alarm_description         = "This metric monitors error with lambda function '${aws_lambda_function.lambda-cleanup.function_name}'"
-  alarm_actions             = ["${var.lambda_alarm_actions}"]
-  dimensions {
-    FunctionName = "${aws_lambda_function.lambda-cleanup.function_name}"
+  alarm_name          = "${aws_lambda_function.lambda-cleanup.function_name}-error"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = var.lambda_alarm_period
+  statistic           = "Sum"
+  threshold           = "1"
+  alarm_description   = "This metric monitors error with lambda function '${aws_lambda_function.lambda-cleanup.function_name}'"
+  alarm_actions       = var.lambda_alarm_actions
+  dimensions = {
+    FunctionName = aws_lambda_function.lambda-cleanup.function_name
   }
 }
+
--- output.tf
+++ output.tf
@@ -1,7 +1,8 @@
 output "aws_iam_role_arn" {
-  value = "${aws_iam_role.lambda.arn}"
+  value = aws_iam_role.lambda.arn
 }
 
 output "lambda_function_name" {
-  value = "${aws_lambda_function.lambda.function_name}"
+  value = aws_lambda_function.lambda.function_name
 }
+
